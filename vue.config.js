dc91b37f168628327523f001035849ac44b72bd8
SolityForFree <strax950@gmail.com>
2024-01-09 17:00:16 +0100

    WIP.

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath : '/EnjoySGameVueUI/dist',
   pwa: {
     workboxOptions: {
         skipWaiting: true
     },
-  publicPath : '/EnjoySGameVueUI/dist'
 }
 })
